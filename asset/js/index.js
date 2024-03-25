0b3193b58a6fe6155e9a7120cd1fab90b15d8185
Kin Justin <rosales.kinoj@gmail.com>
2024-03-25 11:54:52 +0800

    modified contact section

--- asset/js/index.js
+++ asset/js/index.js
@@ -187,3 +187,20 @@ function toggle(event) {
         }
     });
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzyCoVoTFs6p9cgmGH0tzVaKbwrXELRanhkRjOleIqjCIhy4-zAQ18TV8EUlL526rTpSA/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(() => {
+            msg.innerHTML = ""
+        }, 3000);
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
